2ee2ad0f04e7f8412238b19219111e44afd7e0d9
Mahmoud Elshakoushy <Elshakoushy@elshakoushy-fvfvj0ttj1wv.corp.uber.com>
2019-04-12 21:45:10 +0200

    second

--- pixel-city/Controller/MaPVC.swift
+++ pixel-city/Controller/MaPVC.swift
@@ -8,25 +8,48 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     //Outlets
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManger = CLLocationManager()
+    let autherizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManger.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func centerMapBtnPressed(_ sender: Any) {
-        
+        if autherizationStatus == .authorizedAlways || autherizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
-    
 }
 
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManger.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate,latitudinalMeters: regionRadius * 2.0,longitudinalMeters: regionRadius*2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if autherizationStatus == .notDetermined {
+            locationManger.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
